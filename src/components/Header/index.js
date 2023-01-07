c3a0582054c4e6ac4f8f1f58b1c9c05fd0e92bd1
Ariane Sousa <ariane.sousa011@gmail.com>
2023-01-06 23:46:16 -0300

    Visibilidade do header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,6 +3,15 @@ import logo from "../../assets/icons/poker.svg";
 import { Container } from "./Header.styled";
 
 export const Header = () => {
+  // const param = (e) => {
+  //   console.log("PASSOU AQUI");
+  //   if (window.location.pathname === "/") {
+  //     console.log("PASSOU DE NOVO");
+  //     e.preventDefault();
+  //     return true;
+  //   } else return false;
+  // };
+
   return (
     <>
       <Container>
@@ -13,13 +22,15 @@ export const Header = () => {
           <p>PlanningPoker</p>
         </div>
       </Container>
-      <hr
-        style={{
-          borderTop: "1.6px solid #2f3640",
-          color: "#2f3640",
-          opacity: 0.1,
-        }}
-      />
+      {window.location.pathname !== "/" && (
+        <hr
+          style={{
+            borderTop: "1.6px solid #2f3640",
+            color: "#2f3640",
+            opacity: 0.1,
+          }}
+        />
+      )}
     </>
   );
 };

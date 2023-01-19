4e3cec58080f2a5c5f64fa96343326863dc0918f
Ariane Sousa <ariane.sousa011@gmail.com>
2023-01-18 23:15:24 -0300

    Definindo tamanho altura de div como auto

--- src/components/Pages/Room/Room.styled.js
+++ src/components/Pages/Room/Room.styled.js
@@ -34,7 +34,7 @@ export const Container = styled.div`
     border: 1px solid rgba(0, 0, 0, 0.25);
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     border-radius: 10px;
-    height: 541px;
+    height: auto;
     width: 362px;
     margin: 0px 90px 20px 40px;
   }
